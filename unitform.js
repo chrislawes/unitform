1fa27cfeaed3964f2071f2875388b7b141578cd9
Edd Temple-Smithson <edd.temple-smithson@theunit.co.uk>
2014-07-28 16:11:08 +0100

    added focus and blur events as classes on the wrapper element for select, checkbox and radiobutton elements added disabled class to the wrapper for disabled select elements

--- unitform.js
+++ unitform.js
@@ -1,10 +1,9 @@
-
 /*--------------------------------------------------------------------------------------------------------------
 
-		JQuery Unitform Plug-in (v1.0)
+    JQuery Unitform Plug-in (v1.0)
 
-			by Chris Lawes
-			at The Unit (@theunitgb)
+      by Chris Lawes
+      at The Unit (@theunitgb)
 
 --------------------------------------------------------------------------------------------------------------*/
 
@@ -12,176 +11,279 @@ $.fn.unitform = function()
 {
 
 
-	/*----------------------------------------------------------------------------------------------------------
-		SETTINGS
-	----------------------------------------------------------------------------------------------------------*/
+  /*----------------------------------------------------------------------------------------------------------
+    SETTINGS
+  ----------------------------------------------------------------------------------------------------------*/
 
-	// base settings
-	var $self = $(this.selector),
-		inputType;
+  // base settings
+  var $self = $(this.selector),
+    inputType;
 
 
-	/*----------------------------------------------------------------------------------------------------------
-		HELPERS
-	----------------------------------------------------------------------------------------------------------*/
+  /*----------------------------------------------------------------------------------------------------------
+    HELPERS
+  ----------------------------------------------------------------------------------------------------------*/
 
-	// global helper - check if this selector is checked and change parent class
-	// @param {array} selector - jquery selector
-	function ifChecked(selector)
-	{
-		if ($(selector).is(':checked'))
-		{
-			$(selector).parent('span').addClass('checked');
-		}
-		else
-		{
-			$(selector).parent('span').removeClass('checked');
-		}
-	}
+  // global helper - check if this selector is checked and change parent class
+  // @param {array} selector - jquery selector
+  function ifChecked(selector)
+  {
+    if ($(selector).is(':checked'))
+    {
+      $(selector).parent('span').addClass('checked');
+    }
+    else
+    {
+      $(selector).parent('span').removeClass('checked');
+    }
+  }
 
-	// change radio/check markup function 
-	// use: check if already checked (on load) and toggle class
-	// use: bind to click to toggle active class
-	// @param {array} selector - jquery selector
-	function radioCheck_markup(selector)
-	{
+  // change radio/check markup function 
+  // use: check if already checked (on load) and toggle class
+  // use: bind to click to toggle active class
+  // @param {array} selector - jquery selector
+  function radioCheck_markup(selector)
+  {
 
-		// define type of element, used for wrapper claass
-		if ($(selector).is(':radio'))
-		{
-			inputType = 'radio';
-		}
-		else
-		{
-			inputType = 'checkbox';
-		}
+    // define type of element, used for wrapper claass
+    if ($(selector).is(':radio'))
+    {
+      inputType = 'radio';
+    }
+    else
+    {
+      inputType = 'checkbox';
+    }
 
-		// add wrapper
-		$(selector).wrap('<div class="unitform_' + inputType + '"><span></span></div>');
+    // add wrapper
+    $(selector).wrap('<div class="unitform_' + inputType + '"><span></span></div>');
 
-		// if it's already checked, add checked class
-		ifChecked(selector);
+    // if it's already checked, add checked class
+    ifChecked(selector);
 
-	}
+  }
 
-	// change selectbox markup; add wrapper, span for value (+ span for arrow?)
-	// @param {array} selector - jquery selector
-	function select_markup(selector)
-	{
+  // change selectbox markup; add wrapper, span for value (+ span for arrow?)
+  // @param {array} selector - jquery selector
+  function select_markup(selector)
+  {
 
-		// add wrapper and spans for value (and em for arrow)
-		$(selector).wrap('<div class="unitform_select"></div>').parent().append('<span>' + $(selector).find('option:selected').text() + '</span> <em>Arrow</em>');
+    // add wrapper and spans for value (and em for arrow)
+    $(selector).wrap('<div class="unitform_select"></div>').parent().append('<span>' + $(selector).find('option:selected').text() + '</span> <em>Arrow</em>');
 
-		// $("#yourdropdownid option:selected").text();
-		
-		// pinch any class names on the select
-		// remove, and put them on the new 'unitform_select' wrapper
-		
-		var thisSelectClass = $(selector).attr('class');
-		
-		if (thisSelectClass)
-		{
-			$(selector).removeClass(thisSelectClass).parent('.unitform_select').addClass(thisSelectClass);
-		}
+    // $("#yourdropdownid option:selected").text();
+    
+    // pinch any class names on the select
+    // remove, and put them on the new 'unitform_select' wrapper
+    
+    var thisSelectClass = $(selector).attr('class');
+    
+    if (thisSelectClass)
+    {
+      $(selector).removeClass(thisSelectClass).parent('.unitform_select').addClass(thisSelectClass);
+    }
 
-	}
+    // move disabled attribute if present
+    
+    if($(selector).attr('disabled')) 
+    {
+    	$(selector).parent.addClass('disabled');
+    }
 
-	// on change (selectbox) - change class and 1st span content
-	// @param {array} selector - jquery selector
-	function select_onChange(selector)
-	{
-		
-		// find new value, pass to parents span
-		$(selector).parent().find('span').html($(selector).find('option:selected').text());
-		
-	}
+  }
 
-	// on change (radio) - change class of this if the thing (passed in) un/checked
-	// remove checked class from all other radios in this group
-	// @param {array} selector - jquery selector
-	function radio_onChange(selector)
-	{
+  // on change (selectbox) - change class and 1st span content
+  // @param {array} selector - jquery selector
+  function select_onChange(selector)
+  {
+    
+    // find new value, pass to parents span
+    $(selector).parent().find('span').html($(selector).find('option:selected').text());
+    
+  }
 
-		// find the group this radio belongs to (via their shared name)
-		var thisGroupName = $(selector).attr('name');
+  // on focus (selectbox) - change class
+  // @param {array} selector - jquery selector
+  function select_onFocus(selector)
+  {
+    
+    // find new value, pass to parents span
+    $(selector).parent().addClass('focus');
+    
+  }
 
-		// loop all radios with this name, uncheck and remove active class
-		$('input:radio[name=' + thisGroupName +']').each(function()
-		{
-			$(this).prop('checked', false).parent('span').removeClass('checked');
-		});
-		
-		// add active class to the one clicked
-		$(selector).prop('checked', true).parent('span').addClass('checked');
+  // on blur (selectbox) - change class
+  // @param {array} selector - jquery selector
+  function select_onBlur(selector)
+  {
+    
+    // find new value, pass to parents span
+    $(selector).parent().removeClass('focus');
+    
+  }
 
-	}
+  // on change (radio) - change class of this if the thing (passed in) un/checked
+  // remove checked class from all other radios in this group
+  // @param {array} selector - jquery selector
+  function radio_onChange(selector)
+  {
 
-	// on change (checkbox) - change class of this if the thing (passed in) un/checked
-	function check_onChange(selector)
-	{
+    // find the group this radio belongs to (via their shared name)
+    var thisGroupName = $(selector).attr('name');
 
-		// run if checked helper function
-		ifChecked(selector);
+    // loop all radios with this name, uncheck and remove active class
+    $('input:radio[name=' + thisGroupName +']').each(function()
+    {
+      $(this).prop('checked', false).parent('span').removeClass('checked');
+    });
+    
+    // add active class to the one clicked
+    $(selector).prop('checked', true).parent('span').addClass('checked');
 
-	}
+  }
 
-	/*----------------------------------------------------------------------------------------------------------
-		MAIN PLUG-IN
-	----------------------------------------------------------------------------------------------------------*/
+  // on focus (radio) - change class
+  // @param {array} selector - jquery selector
+  function radio_onFocus(selector)
+  {
+    
+    // find new value, pass to parents span
+    $(selector).parent().addClass('focus');
+    
+  }
 
-	// don't run if none of the elements are on this page
-	if ($self.length)
-	{
+  // on blur (radio) - change class
+  // @param {array} selector - jquery selector
+  function radio_onBlur(selector)
+  {
+    
+    // find new value, pass to parents span
+    $(selector).parent().removeClass('focus');
+    
+  }
 
-		// foreach form element passed in
-		$self.each(function()
-		{
+  // on change (checkbox) - change class of this if the thing (passed in) un/checked
+  function check_onChange(selector)
+  {
 
-			// if this is a selectbox
-			if ($(this).is('select'))
-			{
+    // run if checked helper function
+    ifChecked(selector);
 
-				// change markup helper function
-				select_markup(this);
+  }
 
-				// bind change helper function to this
-				$(this).bind('change', function()
-				{
-					select_onChange(this);
-				});
-				
-			}
-			else if ($(this).is(':radio'))
-			{
+  // on focus (check) - change class
+  // @param {array} selector - jquery selector
+  function check_onFocus(selector)
+  {
+    
+    // find new value, pass to parents span
+    $(selector).parent().addClass('focus');
+    
+  }
 
-				// change markup helper function
-				radioCheck_markup(this);
+  // on blur (check) - change class
+  // @param {array} selector - jquery selector
+  function check_onBlur(selector)
+  {
+    
+    // find new value, pass to parents span
+    $(selector).parent().removeClass('focus');
+    
+  }
 
-				// bind to on change function
-				$(this).bind('change', function()
-				{
-					radio_onChange(this);
-				});
+  /*----------------------------------------------------------------------------------------------------------
+    MAIN PLUG-IN
+  ----------------------------------------------------------------------------------------------------------*/
 
-			// else if this is a checkbox
-			}
-			else if ($(this).is(':checkbox'))
-			{
+  // don't run if none of the elements are on this page
+  if ($self.length)
+  {
 
-				// change markup helper function
-				radioCheck_markup(this);
+    // foreach form element passed in
+    $self.each(function()
+    {
 
-				// bind to same on change function
-				$(this).bind('change', function()
-				{
-					check_onChange(this);
-				});
-			
-			}
+      // if this is a selectbox
+      if ($(this).is('select'))
+      {
 
-		});
+        // change markup helper function
+        select_markup(this);
 
-	}
+        // bind change helper function to this
+        $(this).bind('change', function()
+        {
+          select_onChange(this);
+        });
+
+        // bind focus helper function to this
+        $(this).bind('focus', function()
+        {
+          select_onFocus(this);
+        });
+
+        // bind focus helper function to this
+        $(this).bind('blur', function()
+        {
+          select_onBlur(this);
+        });
+        
+      }
+      else if ($(this).is(':radio'))
+      {
+
+        // change markup helper function
+        radioCheck_markup(this);
+
+        // bind to on change function
+        $(this).bind('change', function()
+        {
+          radio_onChange(this);
+        });
+
+        // bind focus helper function to this
+        $(this).bind('focus', function()
+        {
+          radio_onFocus(this);
+        });
+
+        // bind focus helper function to this
+        $(this).bind('blur', function()
+        {
+          radio_onBlur(this);
+        });
+
+      // else if this is a checkbox
+      }
+      else if ($(this).is(':checkbox'))
+      {
+
+        // change markup helper function
+        radioCheck_markup(this);
+
+        // bind to same on change function
+        $(this).bind('change', function()
+        {
+          check_onChange(this);
+        });
+
+        // bind focus helper function to this
+        $(this).bind('focus', function()
+        {
+          check_onFocus(this);
+        });
+
+        // bind focus helper function to this
+        $(this).bind('blur', function()
+        {
+          check_onBlur(this);
+        });
+      
+      }
+
+    });
+
+  }
 
 
 }; // End; unitform plug-in
